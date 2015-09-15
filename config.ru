f277159c7dfb5bb8a50b3a5e9367eb72f94a0407
neurodynamic <neurodynamicdev@gmail.com>
2015-09-15 12:02:57 -0400

    add autoprefixing

--- config.ru
+++ config.ru
@@ -1,6 +1,31 @@
 require 'sass/plugin/rack'
+require "autoprefixer-rails"
+
+class ToPrefixed
+  def initialize(app)
+    @app = app
+  end
+ 
+  def call(env)
+    status, headers, body  = @app.call(env)
+ 
+    css = File.read('public/stylesheets/app.css')
+
+    prefixed_css = AutoprefixerRails.process(css, from: 'app.css').css
+
+    File.open('public/stylesheets/app.css', "wb") do |file|
+      file.write(prefixed_css)
+    end
+
+    [status, headers, body]
+  end
+end
+
+
 
 use Sass::Plugin::Rack
+
+use ToPrefixed
 
 use Rack::Static,
   :urls => ["/images", "/js", "/stylesheets"],
--- public/js/app.js
+++ public/js/app.js
@@ -3,24 +3,24 @@
 var ingredients = [
 {name: 'artichoke heart', minutesMin: 10, minutesMax: 10}, // HSW estimate
 {name: 'asparagus, pieces', minutesMin: 5, minutesMax: 5}, // HSW estimate
-{name: 'green beans', minutesMin: 3, minutesMax: 4}, // HSW estimate
+{name: 'bell peppers', minutesMin: 2, minutesMax: 3}, // HSW estimate
 {name: 'broccoli florets', minutesMin: 3, minutesMax: 4}, // HSW estimate
 {name: 'brussels sprouts, halved', minutesMin: 3, minutesMax: 4}, // HSW estimate
 {name: 'cabbage, shredded', minutesMin: 3, minutesMax: 4}, // HSW estimate
 {name: 'carrots, sliced', minutesMin: 3, minutesMax: 4}, // HSW estimate
 {name: 'cauliflower florets', minutesMin: 3, minutesMax: 4}, // HSW estimate
 {name: 'corn, off the cob', minutesMin: 3, minutesMax: 4}, // HSW, BA summer soba salad
-{name: 'squash, large diced', minutesMin: 4, minutesMax: 6, until: 'softened'}, // BA fresh lemon linguine
 {name: 'eggplant, large diced', minutesMin: 3, minutesMax: 5}, // BA summer ciambotta stew
+{name: 'garlic', minutesMin: 1, minutesMax: 2, until: 'fragrant'}, // BA soba noodle salad
+{name: 'green beans', minutesMin: 3, minutesMax: 4}, // HSW estimate
 {name: 'greens, collard/mustard/turnip', minutesMin: 4, minutesMax: 6}, // HSW estimate
 {name: 'greens, kale/beet', minutesMin: 2, minutesMax: 3}, // HSW estimate
-{name: 'garlic', minutesMin: 1, minutesMax: 2, until: 'fragrant'}, // BA soba noodle salad
 {name: 'mushrooms', minutesMin: 4, minutesMax: 5, until: 'lightly browned'}, // HSW, BA summer bean & mushroom paste
-{name: 'peas', minutesMin: 2, minutesMax: 3}, // HSW estimate
-{name: 'bell peppers', minutesMin: 2, minutesMax: 3}, // HSW estimate
 {name: 'onion', minutesMin: 2, minutesMax: 3}, // my estimate
-{name: 'spinach', minutesMin: 3, minutesMax: 3}, // HSW estimate
+{name: 'peas', minutesMin: 2, minutesMax: 3}, // HSW estimate
 {name: 'potatoes', minutesMin: 12, minutesMax: 15, until: 'browned'}, // Estimate based on BA fried egg-topped summer suate
+{name: 'spinach', minutesMin: 3, minutesMax: 3}, // HSW estimate
+{name: 'squash, large diced', minutesMin: 4, minutesMax: 6, until: 'softened'}, // BA fresh lemon linguine
 {name: 'tofu', minutesMin: 8, minutesMax: 10, until: 'crispy and browned'}, // BA cool long bean & tofu salad
 {name: 'turnips, cubed', minutesMin: 2, minutesMax: 3} // HSW estimate
 ];
@@ -45,9 +45,10 @@ var CookingTimeline = React.createClass({
   ingredient_checkbox: function(ingredient) {
     var element_id = ingredient.name.replace( /(\s+,?)|(\s*,)/, '-' );
     return (
-      <label htmlFor={element_id}>
-        <input id={element_id} type="checkbox" />{ingredient.name}
-      </label>
+      <div>
+        <input id={element_id} type="checkbox" />
+        <label htmlFor={element_id}>{ingredient.name}</label>
+      </div>
     );
   }
 });
