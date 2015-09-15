35cb9a7a678c651cb99226f0b7043faf44a35ad5
neurodynamic <neurodynamicdev@gmail.com>
2015-09-15 16:11:30 -0400

    get chart rerender working

--- public/js/app.js
+++ public/js/app.js
@@ -28,32 +28,106 @@ var ingredients = [
 var CookingTimeline = React.createClass({
   render: function() {
     var self = this;
+    self.renderChart();
 
     return (
       <div className="cookingTimeline">
-        <ul className="ingredients">
-          {
-            ingredients.map(function(ingredient) {
-              return <li key={ingredient.name}>{self.ingredient_checkbox(ingredient)}</li>;
-            })
-          }
-        </ul>
+      <ul className="ingredients" onChange={this.renderChart}>
+      {
+        ingredients.map(function(ingredient) {
+          return <li key={ingredient.name}>{self.ingredientCheckbox(ingredient)}</li>;
+        })
+      }
+      </ul>
       </div>
-    );
+      );
   },
 
-  ingredient_checkbox: function(ingredient) {
+  ingredientCheckbox: function(ingredient) {
     var element_id = ingredient.name.replace( /(\s+,?)|(\s*,)/, '-' );
     return (
       <div>
-        <input id={element_id} type="checkbox" />
-        <label htmlFor={element_id}>{ingredient.name}</label>
+      <input id={element_id} type="checkbox" />
+      <label htmlFor={element_id}>{ingredient.name}</label>
       </div>
-    );
+      );
+  },
+
+  getIngredient: function(ingredientId) {
+    ingredients.find(
+      function(element,index,array) { 
+        return element.name.replace( /(\s+,?)|(\s*,)/, '-' ) == ingredientId;
+      }
+    )
+  },
+
+  renderChart: function() {
+    var self = this;
+
+    var includedIngredients = $('input[type="checkbox"]:checked').map(function(ingredientInput) {
+      var ingredientId = ingredientInput.id;
+      self.getIngredient(ingredientId);
+    });
+
+    console.log('includedIngredients');
+    console.log(includedIngredients);
+
+    $('#container').highcharts({
+
+      chart: { type: 'columnrange', inverted: true },
+
+      title: { text: 'Cooking Times' },
+
+      subtitle: { text: 'Start/Stop' },
+
+      // xAxis: { categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'] },
+      xAxis: { categories: includedIngredients.map(function(ingredient) { return ingredient.name }) },
+
+      yAxis: { title: { text: 'Temperature ( °C )' } },
+
+      tooltip: { valueSuffix: '°C' },
+
+      plotOptions: {
+        columnrange: {
+          dataLabels: {
+            enabled: true,
+            formatter: function () {
+              return this.y + '°C';
+            }
+          }
+        }
+      },
+
+      legend: {
+        enabled: false
+      },
+
+      series: [{
+        name: 'Temperatures',
+        data: [
+        [-9.7, 9.4],
+        [-8.7, 6.5],
+        [-3.5, 9.4],
+        [-1.4, 19.9],
+        [0.0, 22.6],
+        [2.9, 29.5],
+        [9.2, 30.7],
+        [7.3, 26.5],
+        [4.4, 18.0],
+        [-3.1, 11.4],
+        [-5.2, 10.4],
+        [-5.2, 10.4],
+        [-5.2, 10.4],
+        [-5.2, 10.4],
+        [-5.2, 10.4],
+        [-13.5, 9.8]
+        ]
+      }]
+    });
   }
 });
 
 React.render(
   <CookingTimeline />,
   document.getElementById('content')
-);
+  );
